52f2f3f756f20f8e4cf3263e3802da98adb00c57
Melody G <melodyflash@gmail.com>
2023-09-23 13:59:00 -0700

    assignment one completed and uploaded

--- script.js
+++ script.js
@@ -1,3 +1,148 @@
-function sayHello() {
-            alert('Hello world!')
-        }
+//STEP 1 - Convert to Camel Case notation
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let mylibraryFunction
+
+
+
+//Step 2 - Example of numeric literal expression, string literal expession, Boolean literal expression, null literal expression
+// 3.14
+// 'Pi'
+// true
+// null
+
+
+
+//Step 3 - Complex / Variable expessions
+// let anExpression = 4 * 3 * (6/2)
+// let anotherExpression = 4 + (6/2)
+
+
+
+//Step 4 - Declare but do not assign 9 variables - use Camel Casing and Hungarian Notation
+// let firstName, lastName, strAddress, strCity, strState, intZipCode, intYourAge, referralSource, blnMayWeContactYou
+
+
+
+//Step 5 -  3 ways for declaring and assigning values
+// let firstName
+// let lastName
+// let intZipCode
+// firstName = 'Melody'
+// lastName = 'Guzman'
+// intZipCode = 92113
+
+// let firstName = 'Melody'
+// let lastName = 'Guzman'
+// let intZipCode = 92113
+
+// let firstName = 'Melody', let lastName = 'Guzman', let intZipCode = 92113
+
+
+
+//Step 6 - Create a variable, display coerced result in the browser’s console window.
+// x = 10 + 'Hello'
+// console.log(x)
+
+
+
+//Step 7 - 2 variables, add a boolean and a string and a number and a boolean. display
+// y = null + 'hello' 
+// z = 10 + null 
+// console.log(y,z)
+
+
+
+//Step 8 - fix the string
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+// added backslack before '
+
+
+
+//Step 9 - variable that produces nul value in console window, variable that produces an undefined value in the console window
+// let a
+// let b = null
+// console.log(a,b)
+
+
+
+//Step 10 use typeof for string, number, Boolean, object, and undefined
+// typeof 'Melody'
+// typeof 10
+// typeof true
+// typeof console
+// typeof of
+
+
+
+//Step 11 - Within an alert box, use the concatenation operator (+) to display text in the alert box 
+// alert('Hello ' + myName + ',\nWelcome to the ' + course + ' class!') 
+
+
+
+//Step 12 - Declare a variable called name and set it equal to your name, substitute your name in previous step
+// let myName = 'Melody Guzman'
+
+
+
+//Step 13 - Declare a variable called course and set it equal to “JavaScript” substitute in step 11
+// let course = 'JavaScript'
+
+
+
+//Step 14 - Rework the above string so that a line break is added right before the word “Welcome”.
+//see step 11
+
+
+
+//Step 15 - Replace the hardcoded string of your name with a prompt that asks the user for their name. The prompt’s response will now be captured in the name variable.
+// let userName = prompt('What is your name?')
+// alert('Hello ' + userName + ',\nWelcome to the ' + course + ' class!') 
+
+
+
+//Step 16 - Replace the hardcoded string of the class you are taking with a prompt that asks the user for the class they are taking. The prompt’s response will now be captured in the course variable.
+// let courseClass = prompt('What class is this?')
+// alert('Hello ' + userName + ',\nWelcome to the ' + courseClass + ' class!') 
+
+
+
+//Step 17 - declare variable x and variable y add and display in console
+// let x = 10
+// let y = 20
+// console.log(y + x)
+
+
+
+//Step 18 -Declare a variable called x and assign it a value of 20. Add and assign 20 to that variable and display the result in the console window.
+// let x = 20
+// console.log(x + 20)
+
+
+
+//Step 19 - Declare a variable called x and assign it a value of 20. Multiply and assign 5 to that variable and display the result in the console window. 
+// let x = 20
+// console.log(x * 5)
+
+
+
+//Step 20 - Declare a variable called x and assign it a value that equals the remainder of 20 divided by 3.Divide and assign 1 to that variable and display the result in the console window.
+// x = 20 % 3
+// console.log(x / 1)
+
+
+
+//Step 21 - Using a set of Comparison and Logical operators, write an application that evaluates to true and displays the result within the console window.
+// let x = 10
+// let y = 10
+// console.log(x == y && x==10)
+
+
+
+//Step 22
+// let x = 5
+// let y = 6
+// console.log(x + y != 10 || x == 6)
